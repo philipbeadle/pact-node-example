f79820981ee2e0ce3c8ac57af3edd27e363b8177
Philip Beadle <philip.beadle@live.com.au>
2017-12-04 15:08:05 +1100

    Setting up Pact.io to run on CI

--- app/client/spec/PostServiceClient.spec.js
+++ app/client/spec/PostServiceClient.spec.js
@@ -98,7 +98,7 @@ describe('Pact', () => {
         var opts = {
         	pactFilesOrDirs: [path.resolve(__dirname, '../../../pacts')],        // Array of local Pact files or directories containing them. Required.
           pactBroker: 'https://elabor8.pact.dius.com.au/',            // URL to fetch the provider states for the given provider API. Optional.
-          pactBrokerUsername: 'sSR6cB6Jt8UE8dyx5pc5QD7V8x2Of',    // Username for Pact Broker basic authentication. Optional
+          pactBrokerUsername: 'sSR6cB6Jt8UE8dyx5pc5QD7V8x2Of',    //    Username for Pact Broker basic authentication. Optional
 	        pactBrokerPassword: "krnrmVVeYYDEMmjeysh8r8GoK0iktOm",
           consumerVersion: '1.0.0'        // A string containing a semver-style version e.g. 1.0.0. Required.
         };
